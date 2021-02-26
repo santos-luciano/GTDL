2d974646b48a59d4647fcc45f379f6ccd2b7f827
santos-luciano <santos.luciano@ufba.br>
2021-02-26 08:27:46 -0300

    GTDL

---
+++ R/artset1987.R
@@ -0,0 +1,12 @@
+#' Arset1987
+#' 
+#' Contains the times to failure of 50 devices put on life test at time 0.
+#' 
+#'@format A data frame with 50 observations on 1 variable
+#'\describe{
+#'\item{t}{Life test at time 0}}
+#'
+#'
+#'@examples
+#'
+#'dGTDL(t = artset1987,lambda = 1,alpha = -0.05,gamma = -1)
---
+++ data-raw/artset187.R
@@ -0,0 +1,3 @@
+u## code to prepare `artset187` dataset goes here
+
+usethis::use_data(artset187, overwrite = TRUE)
